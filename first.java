392b7a07d8f6ad2fc61cc930df0239f888bbab3c
varunnanjappa1 <amisvarunnanjappa@gmail.com>
2022-11-27 13:39:13 +0530

    minor

--- first.java
+++ first.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class first1 {
+    
     public static void main(String args[]) {
         Scanner read = new Scanner(System.in);
         System.out.print("enter a number");
@@ -11,7 +12,30 @@ class first1 {
                 System.out.print(" *");
             System.out.println();
         }
-           
+        for ( j=1;j<=n;j++){
+            for( i=1;i<=n;i++){
+                if(i==n||j==n||i==1||j==1)
+                    System.out.print("  *");
+                else
+                    System.out.print("   ");        
+            }
+            System.out.println();
+        }   
+        for(i=n;i>=1;i--){
+            for(j=1;j<=i;j++)
+                System.out.print(" #");
+            System.out.println();
+        }
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" #");
+            }
+            System.out.println();
+        }
+        
                 
     }
 }
